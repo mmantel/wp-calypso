1100907ee3f591e218e018d8d18073aec4cd7d51
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-07-06 13:37:31 -0300

    Comments: connects the like button to the store

--- client/my-sites/comments/comment-list/index.jsx
+++ client/my-sites/comments/comment-list/index.jsx
@@ -311,6 +311,7 @@ export class CommentList extends Component {
 							replyComment={ this.replyComment }
 							setCommentStatus={ this.setCommentStatus }
 							siteId={ siteId }
+							submitComment={ this.submitComment }
 							toggleCommentLike={ this.toggleCommentLike }
 							toggleCommentSelected={ this.toggleCommentSelected }
 						/>
